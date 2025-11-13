697a67e5b0ebb855d6db12711020ef5af9555b2f
dplochcoder <daniel.ploch.coder@gmail.com>
2025-11-13 14:17:46 -0500

    Address comments on packaging and props. Switch from MMHook to MonoDetour.

--- Internal/GameObjectUtil.cs
+++ Internal/GameObjectUtil.cs
@@ -4,6 +4,16 @@ namespace Silksong.ModMenu.Internal;
 
 internal static class GameObjectUtil
 {
+    // TODO: Move this to UnityHelper and use that.
+    internal static GameObject? FindChild(this GameObject self, string path)
+    {
+        var t = self.transform.Find(path);
+        if (t == null)
+            return null;
+
+        return t.gameObject;
+    }
+
     internal static void DestroyAllChildren(this GameObject self)
     {
         for (int i = self.transform.childCount - 1; i >= 0; i--)
