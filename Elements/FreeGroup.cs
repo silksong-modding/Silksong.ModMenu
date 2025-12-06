deaef82f30135a152a860e20432330227de1d6b7
dplochcoder <daniel.ploch.coder@gmail.com>
2025-12-05 20:42:45 -0500

    Implement free placement panel

---
+++ Elements/FreeGroup.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Silksong.ModMenu.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Silksong.ModMenu.Elements;
+
+/// <summary>
+/// An arbitrary collection of menu elements where each has a manually assigned position.
+/// Navigation to defaults to the single left/right/top/bottom-most element within.
+/// </summary>
+public class FreeGroup : INavigableMenuEntity
+{
+    private readonly VisibilityManager visibility = new();
+    private readonly LinkedDictionary<IMenuEntity, Vector2> entities = [];
+
+    /// <inheritdoc/>
+    public VisibilityManager Visibility => visibility;
+
+    /// <inheritdoc/>
+    public IEnumerable<MenuElement> AllElements() => entities.Keys.SelectMany(e => e.AllElements());
+
+    /// <inheritdoc/>
+    public void ClearNeighbor(NavigationDirection direction)
+    {
+        if (!GetSelectable(direction, out var selectable))
+            return;
+
+        SelectableWrapper wrapper = new(selectable);
+        wrapper.ClearNeighbor(direction);
+    }
+
+    /// <inheritdoc/>
+    public void ClearNeighbors()
+    {
+        ClearNeighbor(NavigationDirection.Up);
+        ClearNeighbor(NavigationDirection.Left);
+        ClearNeighbor(NavigationDirection.Right);
+        ClearNeighbor(NavigationDirection.Down);
+    }
+
+    /// <inheritdoc/>
+    public SelectableElement? GetDefaultSelectable() =>
+        entities
+            .Keys.OfType<INavigableMenuEntity>()
+            .Select(e => e.GetDefaultSelectable())
+            .Where(s => s != null)
+            .FirstOrDefault();
+
+    private static float SortKey(NavigationDirection direction, Vector2 pos) =>
+        direction switch
+        {
+            NavigationDirection.Up => pos.y,
+            NavigationDirection.Left => -pos.x,
+            NavigationDirection.Right => pos.x,
+            NavigationDirection.Down => -pos.y,
+            _ => throw new ArgumentException($"{direction}"),
+        };
+
+    /// <inheritdoc/>
+    public bool GetSelectable(
+        NavigationDirection direction,
+        [MaybeNullWhen(false)] out Selectable selectable
+    )
+    {
+        selectable = entities
+            .OrderBy(e => SortKey(direction, e.Value))
+            .Select(e =>
+                ((e.Key as INavigable)?.GetSelectable(direction, out var s) ?? false) ? s : null
+            )
+            .Where(s => s != null)
+            .FirstOrDefault();
+        return selectable != null;
+    }
+
+    private GameObject? gameObjectParent;
+
+    /// <inheritdoc/>
+    public void SetGameObjectParent(GameObject parent)
+    {
+        if (gameObjectParent != null)
+            throw new ArgumentException("GameObjectParent already set");
+
+        gameObjectParent = parent;
+        foreach (var entity in entities.Keys)
+            entity.SetGameObjectParent(gameObjectParent);
+    }
+
+    /// <inheritdoc/>
+    public void SetMenuParent(IMenuEntity parent) => visibility.SetParent(parent.Visibility);
+
+    /// <inheritdoc/>
+    public void SetNeighbor(NavigationDirection direction, Selectable selectable)
+    {
+        if (!GetSelectable(direction, out var mine))
+            return;
+
+        SelectableWrapper wrapper = new(mine);
+        wrapper.SetNeighbor(direction, selectable);
+    }
+
+    /// <inheritdoc/>
+    public void UpdateLayout(Vector2 localAnchorPos)
+    {
+        foreach (var e in entities)
+            e.Key.UpdateLayout(localAnchorPos + e.Value);
+    }
+}
---
+++ Internal/LinkedDictionary.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Silksong.ModMenu.Internal;
+
+internal class LinkedDictionary<K, V> : IDictionary<K, V>
+{
+    private readonly Dictionary<K, (LinkedListNode<K>, V)> data = [];
+    private readonly LinkedList<K> keyOrder = [];
+
+    public V this[K key]
+    {
+        get => data[key].Item2;
+        set => data[key] = (data[key].Item1, value);
+    }
+
+    public ICollection<K> Keys => new LinkedDictionaryKeys(this);
+
+    public ICollection<V> Values => new LinkedDictionaryValues(this);
+
+    public int Count => keyOrder.Count;
+
+    public bool IsReadOnly => false;
+
+    public void Add(K key, V value) =>
+        data[key] = (
+            data.TryGetValue(key, out var pair) ? pair.Item1 : keyOrder.AddLast(key),
+            value
+        );
+
+    public void Add(KeyValuePair<K, V> item) => Add(item.Key, item.Value);
+
+    public void Clear()
+    {
+        data.Clear();
+        keyOrder.Clear();
+    }
+
+    public bool Contains(KeyValuePair<K, V> item) =>
+        data.TryGetValue(item.Key, out var pair)
+        && EqualityComparer<V>.Default.Equals(item.Value, pair.Item2);
+
+    public bool ContainsKey(K key) => data.ContainsKey(key);
+
+    public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+    {
+        foreach (var key in keyOrder)
+            array[arrayIndex++] = new(key, data[key].Item2);
+    }
+
+    public IEnumerator<KeyValuePair<K, V>> GetEnumerator() =>
+        keyOrder.Select(k => new KeyValuePair<K, V>(k, data[k].Item2)).GetEnumerator();
+
+    public bool Remove(K key)
+    {
+        if (!data.TryGetValue(key, out var pair))
+            return false;
+
+        data.Remove(key);
+        keyOrder.Remove(pair.Item1);
+        return true;
+    }
+
+    public bool Remove(KeyValuePair<K, V> item)
+    {
+        if (
+            !data.TryGetValue(item.Key, out var pair)
+            || !EqualityComparer<V>.Default.Equals(item.Value, pair.Item2)
+        )
+            return false;
+
+        data.Remove(item.Key);
+        keyOrder.Remove(pair.Item1);
+        return true;
+    }
+
+    public bool TryGetValue(K key, out V value)
+    {
+        if (!data.TryGetValue(key, out var pair))
+        {
+            value = default;
+            return false;
+        }
+
+        value = pair.Item2;
+        return true;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private class LinkedDictionaryKeys(LinkedDictionary<K, V> parent) : ICollection<K>
+    {
+        public int Count => parent.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(K item) => throw new InvalidOperationException();
+
+        public void Clear() => parent.Clear();
+
+        public bool Contains(K item) => parent.ContainsKey(item);
+
+        public void CopyTo(K[] array, int arrayIndex)
+        {
+            foreach (var key in parent.keyOrder)
+                array[arrayIndex++] = key;
+        }
+
+        public IEnumerator<K> GetEnumerator() => parent.keyOrder.GetEnumerator();
+
+        public bool Remove(K item) => parent.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    private class LinkedDictionaryValues(LinkedDictionary<K, V> parent) : ICollection<V>
+    {
+        public int Count => parent.Count;
+
+        public bool IsReadOnly => false;
+
+        public void Add(V item) => throw new InvalidOperationException(nameof(Add));
+
+        public void Clear() => parent.Clear();
+
+        public bool Contains(V item) =>
+            parent.data.Any(e => EqualityComparer<V>.Default.Equals(e.Value.Item2, item));
+
+        public void CopyTo(V[] array, int arrayIndex)
+        {
+            foreach (var key in parent.keyOrder)
+                array[arrayIndex++] = parent.data[key].Item2;
+        }
+
+        public IEnumerator<V> GetEnumerator() =>
+            parent.keyOrder.Select(k => parent.data[k].Item2).GetEnumerator();
+
+        public bool Remove(V item)
+        {
+            foreach (var e in parent.data)
+            {
+                if (!EqualityComparer<V>.Default.Equals(e.Value.Item2, item))
+                    continue;
+
+                parent.Remove(e.Key);
+                return true;
+            }
+
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
