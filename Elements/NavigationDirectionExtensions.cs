a40dcc694147dc5569055f3eb73311b0155e6be6
dplochcoder <daniel.ploch.coder@gmail.com>
2025-12-06 14:55:09 -0500

    Improve UpdateLayout documentation, fix VerticalGroup.

---
+++ Elements/NavigationDirectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Silksong.ModMenu.Elements;
+
+/// <summary>
+/// Helper functions for working with navigation directions.
+/// </summary>
+public static class NavigationDirectionExtensions
+{
+    /// <summary>
+    /// Get the opposite direction of this one.
+    /// </summary>
+    public static NavigationDirection Opposite(this NavigationDirection self) =>
+        self switch
+        {
+            NavigationDirection.Up => NavigationDirection.Down,
+            NavigationDirection.Left => NavigationDirection.Right,
+            NavigationDirection.Right => NavigationDirection.Left,
+            NavigationDirection.Down => NavigationDirection.Up,
+            _ => throw new ArgumentException($"{self}"),
+        };
+}
