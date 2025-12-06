4b21521bc25a6d3800e62bc8e53e8764ca5519e0
dplochcoder <daniel.ploch.coder@gmail.com>
2025-12-06 14:55:09 -0500

    Implement ListView for unique random-access patterns.

---
+++ Internal/ListView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Silksong.ModMenu.Internal;
+
+// Convenience class for random-access of peculiarly organized data.
+internal class ListView<T>(Func<int, T> getter, int count) : IReadOnlyList<T>
+{
+    public T this[int index] => getter(index);
+
+    public int Count => count;
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+            yield return this[i];
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
