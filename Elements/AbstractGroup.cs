9ca31c57c7fcc9b6644579bfffaad19cfef7eb71
dplochcoder <daniel.ploch.coder@gmail.com>
2025-12-06 15:25:30 -0500

    Create AbstractGroup to reduce code reuse.

---
+++ Elements/AbstractGroup.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Silksong.ModMenu.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Silksong.ModMenu.Elements;
+
+/// <summary>
+/// Common functionality shared by most entity groups.
+/// </summary>
+public abstract class AbstractGroup : INavigableMenuEntity
+{
+    private readonly VisibilityManager visibility = new();
+
+    /// <inheritdoc/>
+    public VisibilityManager Visibility => visibility;
+
+    /// <summary>
+    /// Enumerate all child entities within this group.
+    /// </summary>
+    protected abstract IEnumerable<IMenuEntity> AllEntities();
+
+    /// <inheritdoc/>
+    public IEnumerable<MenuElement> AllElements() => AllEntities().SelectMany(e => e.AllElements());
+
+    /// <summary>
+    /// Record `entity` as a child of this group. The group is responsible for storing it in whatever data structure makes sense for it.
+    /// </summary>
+    protected void ParentEntity(IMenuEntity entity)
+    {
+        entity.SetMenuParent(this);
+        if (gameObjectParent != null)
+            entity.SetGameObjectParent(gameObjectParent);
+    }
+
+    /// <summary>
+    /// Enumerate all navigables which should be directly connected in `direction`.
+    /// </summary>
+    protected abstract IEnumerable<INavigable> GetNavigables(NavigationDirection direction);
+
+    /// <inheritdoc/>
+    public virtual void ClearNeighbor(NavigationDirection direction)
+    {
+        foreach (var navigable in GetNavigables(direction))
+            navigable.ClearNeighbor(direction);
+    }
+
+    /// <inheritdoc/>
+    public virtual void ClearNeighbors()
+    {
+        ClearNeighbor(NavigationDirection.Up);
+        ClearNeighbor(NavigationDirection.Left);
+        ClearNeighbor(NavigationDirection.Right);
+        ClearNeighbor(NavigationDirection.Down);
+    }
+
+    /// <inheritdoc/>
+    public virtual SelectableElement? GetDefaultSelectable() =>
+        AllEntities()
+            .Where(e => e.VisibleSelf)
+            .OfType<INavigableMenuEntity>()
+            .Select(n => n.GetDefaultSelectable())
+            .WhereNonNull()
+            .FirstOrDefault();
+
+    /// <inheritdoc/>
+    public abstract bool GetSelectable(
+        NavigationDirection direction,
+        [MaybeNullWhen(false)] out Selectable selectable
+    );
+
+    private GameObject? gameObjectParent;
+
+    /// <inheritdoc/>
+    public virtual void SetGameObjectParent(GameObject parent)
+    {
+        if (gameObjectParent != null)
+            throw new ArgumentException("GameObjectParent already set");
+
+        gameObjectParent = parent;
+        foreach (var entity in AllEntities())
+            entity.SetGameObjectParent(gameObjectParent);
+    }
+
+    /// <inheritdoc/>
+    public virtual void SetMenuParent(IMenuEntity parent) =>
+        visibility.SetParent(parent.Visibility);
+
+    /// <inheritdoc/>
+    public virtual void SetNeighbor(NavigationDirection direction, Selectable selectable)
+    {
+        foreach (var navigable in GetNavigables(direction))
+            navigable.SetNeighbor(direction, selectable);
+    }
+
+    /// <inheritdoc/>
+    public abstract void UpdateLayout(Vector2 localAnchorPos);
+}
--- Elements/FreeGroup.cs
+++ Elements/FreeGroup.cs
@@ -12,43 +12,12 @@ namespace Silksong.ModMenu.Elements;
 /// An arbitrary collection of menu elements where each has a manually assigned position.
 /// Navigation to defaults to the single left/right/top/bottom-most element within.
 /// </summary>
-public class FreeGroup : INavigableMenuEntity
+public class FreeGroup : AbstractGroup
 {
-    private readonly VisibilityManager visibility = new();
     private readonly LinkedDictionary<IMenuEntity, Vector2> entities = [];
 
     /// <inheritdoc/>
-    public VisibilityManager Visibility => visibility;
-
-    /// <inheritdoc/>
-    public IEnumerable<MenuElement> AllElements() => entities.Keys.SelectMany(e => e.AllElements());
-
-    /// <inheritdoc/>
-    public void ClearNeighbor(NavigationDirection direction)
-    {
-        if (!GetSelectable(direction, out var selectable))
-            return;
-
-        SelectableWrapper wrapper = new(selectable);
-        wrapper.ClearNeighbor(direction);
-    }
-
-    /// <inheritdoc/>
-    public void ClearNeighbors()
-    {
-        ClearNeighbor(NavigationDirection.Up);
-        ClearNeighbor(NavigationDirection.Left);
-        ClearNeighbor(NavigationDirection.Right);
-        ClearNeighbor(NavigationDirection.Down);
-    }
-
-    /// <inheritdoc/>
-    public SelectableElement? GetDefaultSelectable() =>
-        entities
-            .Keys.OfType<INavigableMenuEntity>()
-            .Select(e => e.GetDefaultSelectable())
-            .WhereNonNull()
-            .FirstOrDefault();
+    protected override IEnumerable<IMenuEntity> AllEntities() => entities.Keys;
 
     /// <summary>
     /// Add an entity to this free group at the specified position.
@@ -56,10 +25,7 @@ public class FreeGroup : INavigableMenuEntity
     public void Add(IMenuEntity entity, Vector2 offset)
     {
         entities.Add(entity, offset);
-        entity.SetMenuParent(this);
-
-        if (gameObjectParent != null)
-            entity.SetGameObjectParent(gameObjectParent);
+        ParentEntity(entity);
     }
 
     /// <summary>
@@ -73,6 +39,7 @@ public class FreeGroup : INavigableMenuEntity
         entities[entity] = offset;
     }
 
+    // Sort low values first.
     private static float SortKey(NavigationDirection direction, Vector2 pos) =>
         direction switch
         {
@@ -84,49 +51,32 @@ public class FreeGroup : INavigableMenuEntity
         };
 
     /// <inheritdoc/>
-    public bool GetSelectable(
+    public override bool GetSelectable(
         NavigationDirection direction,
         [MaybeNullWhen(false)] out Selectable selectable
     )
     {
-        selectable = entities
-            .OrderBy(e => SortKey(direction, e.Value))
-            .Select(e =>
-                ((e.Key as INavigable)?.GetSelectable(direction, out var s) ?? false) ? s : null
-            )
-            .WhereNonNull()
+        selectable = GetNavigables(direction)
+            .Select(n => n.GetSelectable(direction, out var s) ? s : null)
             .FirstOrDefault();
         return selectable != null;
     }
 
-    private GameObject? gameObjectParent;
-
     /// <inheritdoc/>
-    public void SetGameObjectParent(GameObject parent)
+    protected override IEnumerable<INavigable> GetNavigables(NavigationDirection direction)
     {
-        if (gameObjectParent != null)
-            throw new ArgumentException("GameObjectParent already set");
+        var validEntities = entities.Where(e => e.Key.VisibleSelf && e.Key is INavigable);
+        if (!validEntities.Select(e => SortKey(direction, e.Value)).TryGetMin(out var key))
+            return [];
 
-        gameObjectParent = parent;
-        foreach (var entity in entities.Keys)
-            entity.SetGameObjectParent(gameObjectParent);
+        return validEntities
+            .Where(e => SortKey(direction, e.Value) == key)
+            .Select(e => e.Key as INavigable)!
+            .Take(1);
     }
 
     /// <inheritdoc/>
-    public void SetMenuParent(IMenuEntity parent) => visibility.SetParent(parent.Visibility);
-
-    /// <inheritdoc/>
-    public void SetNeighbor(NavigationDirection direction, Selectable selectable)
-    {
-        if (!GetSelectable(direction, out var mine))
-            return;
-
-        SelectableWrapper wrapper = new(mine);
-        wrapper.SetNeighbor(direction, selectable);
-    }
-
-    /// <inheritdoc/>
-    public void UpdateLayout(Vector2 localAnchorPos)
+    public override void UpdateLayout(Vector2 localAnchorPos)
     {
         ClearNeighbors();
         foreach (var e in entities)
