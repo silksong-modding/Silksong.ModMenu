49ebe33c08630fb95f269d21a4be972e9d54c4f9
Daniel Ploch <daniel.ploch.coder@gmail.com>
2026-04-05 15:58:34 -0400

    Implement a Tree collection utility.

---
+++ Silksong.ModMenu/Internal/Tree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Silksong.ModMenu.Internal;
+
+/// <summary>
+/// A recursive tree structure which supports lazy node creation and postfix traversal.
+/// </summary>
+internal class Tree<K, V>
+    where V : new()
+{
+    private readonly Dictionary<K, Tree<K, V>> subtrees = [];
+
+    public V Value = new();
+
+    public IReadOnlyDictionary<K, Tree<K, V>> Subtrees => subtrees;
+
+    public Tree<K, V> this[IEnumerable<K> keys]
+    {
+        get
+        {
+            Tree<K, V> tree = this;
+            foreach (var key in keys)
+            {
+                if (tree.subtrees.TryGetValue(key, out var subtree))
+                    tree = subtree;
+                else
+                {
+                    subtree = new();
+                    tree.subtrees[key] = subtree;
+                    tree = subtree;
+                }
+            }
+            return tree;
+        }
+    }
+
+    private void ForEachPostfixRecursive(List<K> keys, Action<IReadOnlyList<K>, Tree<K, V>> action)
+    {
+        foreach (var e in subtrees)
+        {
+            keys.Add(e.Key);
+            e.Value.ForEachPostfixRecursive(keys, action);
+            keys.RemoveAt(keys.Count - 1);
+        }
+        action(keys, this);
+    }
+
+    public void ForEachPostfix(Action<IReadOnlyList<K>, Tree<K, V>> action) =>
+        ForEachPostfixRecursive([], action);
+}
